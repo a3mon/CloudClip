858c0a792eb0267dc415cc10729d0d5fc8c31948
Angelo Hannes <Angelo.Hannes@gmx.com>
2014-02-25 08:42:07 +0100

    added JUnit test suite to CloudClipServer
    Added a new Sourse Folder "test" to the CloudClipServer project and added
    the first Test class for model.ClipBoard.

---
+++ CloudClipServer/test/d3v/cc/server/model/ClipBoardTest.java
@@ -0,0 +1,14 @@
+package d3v.cc.server.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ClipBoardTest {
+
+	@Test
+	public void testAddClip() {
+		fail("Not yet implemented");
+	}
+
+}
